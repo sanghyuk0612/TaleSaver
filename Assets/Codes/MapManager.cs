ac4b0d649c94d2a9170b8400b470301ea47aa885
sanghyuk0612 <hwhwhw944@gmail.com>
2025-04-03 15:03:08 +0900

    Update MapManager.cs

--- Assets/Codes/MapManager.cs
+++ Assets/Codes/MapManager.cs
@@ -64,11 +64,12 @@ public class MapManager : MonoBehaviour
         }
     }
     GameObject wallPrefab;
+    GameObject BossMapPrefab;
 
     private void LoadMapPrefabs()
     {
         GameObject[] loadedPrefabs= Resources.LoadAll<GameObject>("Prefabs/Map/Cave");
-        location =4;
+        location =5;
         List<GameObject> filteredPrefabs = new List<GameObject>();
 
         switch(location){
@@ -99,15 +100,17 @@ public class MapManager : MonoBehaviour
         string prefabPath = prefab.name; // Resources.LoadAll은 폴더 정보를 주지 않음 (이름만 가져옴)
         
         // Cave 관련 프리팹을 제외하는 조건 (예: "Cave_"로 시작하는 이름 제외)
-        if (!prefabPath.EndsWith("(wall)")) 
+        if (!prefabPath.EndsWith("(wall)")&&!prefabPath.EndsWith("boss map") ) 
         {
             filteredPrefabs.Add(prefab);
             // 프리팹을 인스턴스화하여 게임 오브젝트를 생성
-            
         }
-        else{
-            
+        else if(prefabPath.EndsWith("(wall)")){
             wallPrefab =prefab;
+        }
+        else if(prefabPath.EndsWith("boss map")){
+            Debug.Log("hi");
+            BossMapPrefab = prefab;
         }
     }
         
