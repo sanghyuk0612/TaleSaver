3d7531a76dc07c7ebdf1484e6babce94b0e6b6f6
Yubeen25 <122802925+Yubeen25@users.noreply.github.com>
2025-04-14 14:35:19 +0900

    Revert "로그인 수정"
    This reverts commit 935e961199a6a81d08b00734f665b34686285819, reversing
    changes made to 692ba877f7917c30fb1d171998a52cac092463c4.

--- Assets/Codes/LoginPanelManager.cs
+++ Assets/Codes/LoginPanelManager.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,24 +12,9 @@ public class LoginPanelManager : MonoBehaviour
     public GameObject gameOptionButton;
     public GameObject gameLoginButton;
     public GameObject gameExitButton;
-    public TMP_InputField emailInput;
+    public TMP_InputField emailInput; // 또는 TMP_InputField
     public TMP_InputField passwordInput;
-    public TMP_Text alertText;
     public FirebaseAuthManager authManager;
-    public TMP_Text loginWarningText;
-
-    void Start()
-    {
-        // 게임 시작 시 로그인 상태 확인
-        if (authManager != null && authManager.IsLoggedIn())
-        {
-            loginWarningText.gameObject.SetActive(false);
-        }
-        else
-        {
-            loginWarningText.gameObject.SetActive(true);
-        }
-    }
 
     public void ShowLoginPanel()
     {
@@ -39,7 +23,6 @@ public class LoginPanelManager : MonoBehaviour
         gameOptionButton.SetActive(false);
         gameLoginButton.SetActive(false);
         gameExitButton.SetActive(false);
-        alertText.text = "";
     }
 
     public void HideLoginPanel()
@@ -53,59 +36,26 @@ public class LoginPanelManager : MonoBehaviour
 
     public void OnClickLogin()
     {
-        Debug.Log(" 로그인 버튼 눌림");
-
         string email = emailInput.text;
         string password = passwordInput.text;
 
-        if (authManager == null)
-        {
-            Debug.LogError("? authManager가 null입니다! Inspector 연결 누락");
-            return;
-        }
-
-        authManager.Login(email, password, (success, message) =>
-        {
-            if (success)
-            {
-                Debug.Log(" 로그인 성공 콜백");
-                alertText.text = " 로그인에 성공하였습니다.";
-                loginWarningText.gameObject.SetActive(false); // ? 경고 숨기기
-                Invoke(nameof(HideLoginPanel), 1.0f);
-            }
-            else
-            {
-                Debug.LogError(" 로그인 실패 콜백: " + message);
-                alertText.text = " " + message;
-            }
-        });
+        authManager.Login(email, password);
     }
 
     public void OnClickSignup()
     {
-        Debug.Log(" 회원가입 버튼 눌림");
+        Debug.Log("회원가입 버튼 눌림"); // ?? 이거 안 뜨면 버튼 연결 안 된 거야
 
         string email = emailInput.text;
         string password = passwordInput.text;
 
         if (authManager == null)
         {
-            Debug.LogError(" authManager가 null입니다! Inspector 연결 누락");
+            Debug.LogError("authManager가 null입니다! Inspector 연결 누락");
             return;
         }
 
-        authManager.SignUp(email, password, (success, message) =>
-        {
-            if (success)
-            {
-                Debug.Log(" 회원가입 성공 콜백");
-                alertText.text = " 회원가입 되었습니다.";
-            }
-            else
-            {
-                Debug.LogError(" 회원가입 실패 콜백: " + message);
-                alertText.text = " " + message;
-            }
-        });
+        authManager.SignUp(email, password);
     }
+
 }
