596edc25e0efcb48876e3f9bc489adbf709fbc51
[jiwon1605] <a01039574485@gmail.com>
2025-05-22 19:18:18 +0900

    옵션시 게임 정지

---
+++ Assets/Codes/SettingPanelController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SettingPanelController : MonoBehaviour
+{
+    public GameObject settingPanel;  // 인스펙터에서 설정
+    private bool isPanelOpen = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleSettingPanel();
+        }
+    }
+
+    public void ToggleSettingPanel()
+    {
+        isPanelOpen = !isPanelOpen;
+        settingPanel.SetActive(isPanelOpen);
+
+        // 게임 일시정지/재개
+        Time.timeScale = isPanelOpen ? 0f : 1f;
+    }
+
+    // 닫기 버튼에서 호출할 수 있도록 별도 메서드
+    public void ClosePanel()
+    {
+        isPanelOpen = false;
+        settingPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
